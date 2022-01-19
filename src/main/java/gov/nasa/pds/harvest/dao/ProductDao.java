7d35d1876e80b35b64a1859e670ac1a6b9e5844f
Eugene <tumbao72@yahoo.com>
2022-01-18 17:40:36 -0800

    Product DAO

--- src/main/java/gov/nasa/pds/harvest/dao/ProductDao.java
+++ src/main/java/gov/nasa/pds/harvest/dao/ProductDao.java
@@ -2,6 +2,9 @@ package gov.nasa.pds.harvest.dao;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,39 +30,19 @@ public class ProductDao
     
     private RestClient client;
     private String indexName;
-    private boolean pretty;
 
-    private EsRequestBuilder requestBld;
-    private SearchResponseParser parser;
-
-    
-    /**
-     * Constructor
-     * @param client Elasticsearch client
-     * @param indexName Elasticsearch index name, e.g., "registry".
-     */
-    public ProductDao(RestClient client, String indexName)
-    {
-        this(client, indexName, false);
-    }
-    
     
     /**
      * Constructor.
      * @param client Elasticsearch client
      * @param indexName Elasticsearch index name, e.g., "registry".
-     * @param pretty Pretty-format Elasticsearch response JSON. Used for debugging.
      */
-    public ProductDao(RestClient client, String indexName, boolean pretty)
+    public ProductDao(RestClient client, String indexName)
     {
         log = LogManager.getLogger(this.getClass());
         
         this.client = client;
         this.indexName = indexName;
-        this.pretty = pretty;
-        
-        requestBld = new EsRequestBuilder();
-        parser = new SearchResponseParser();
     }
 
     
@@ -129,6 +112,163 @@ public class ProductDao
     }
     
     
+    /**
+     * Get number of reference documents (pages) by collection LIDVID and type.
+     * @param collectionLidVid collection LIDVID
+     * @param type reference type: 'P' - primary, 'S' - secondary
+     * @return number of documents (pages)
+     * @throws Exception an exception
+     */
+    public int getRefDocCount(String collectionLidVid, char type) throws Exception
+    {
+        if(collectionLidVid == null) return 0;
+        
+        String strType = null;
+        if(type == 'P')
+        {
+            strType = "primary";
+        }
+        else if(type == 'S')
+        {
+            strType = "secondary";
+        }
+        else
+        {
+            throw new IllegalArgumentException("Invalid type " + type);
+        }
+        
+        // Elasticsearch "Lucene" query
+        String query = "collection_lidvid:\"" + collectionLidVid + "\" AND reference_type:" + strType;
+        query = URLEncoder.encode(query, "UTF-8");
+        log.debug(query);
+        
+        // Request URL
+        String reqUrl = "/" + indexName + "-refs/_count?q=" + query;
+        Request req = new Request("GET", reqUrl);
+        Response resp = null;
+
+        try
+        {
+            resp = client.performRequest(req);
+        }
+        catch(ResponseException ex)
+        {
+            resp = ex.getResponse();
+            int code = resp.getStatusLine().getStatusCode();
+            // Invalid LIDVID
+            if(code == 404 || code == 405) 
+            {
+                return 0;
+            }
+            else
+            {
+                throw ex;
+            }
+        }
+
+        InputStream is = null;
+
+        try
+        {
+            is = resp.getEntity().getContent();
+            JsonReader rd = new JsonReader(new InputStreamReader(is));
+            
+            rd.beginObject();
+            
+            while(rd.hasNext() && rd.peek() != JsonToken.END_OBJECT)
+            {
+                String name = rd.nextName();
+                if("count".equals(name))
+                {
+                    return rd.nextInt();
+                }
+                else
+                {
+                    rd.skipValue();
+                }
+            }
+            
+            rd.endObject();
+        }
+        finally
+        {
+            CloseUtils.close(is);
+        }
+        
+        return 0;
+    }
+    
+    
+    /**
+     * Get product references from collection inventory
+     * @param collectionLidVid collection LIDVID
+     * @param type reference type: 'P' - primary, 'S' - secondary
+     * @param page page number starting from 1
+     * @return list of product LIDVIDs
+     * @throws Exception ResponseException exception - there was a problem calling Elasticsearch,
+     * other exceptions - parsing problems (invalid JSON).
+     */
+    public List<String> getRefs(String collectionLidVid, char type, int page) throws Exception
+    {
+        if(collectionLidVid == null) return null;
+        
+        String docId = collectionLidVid + "::" + type + page;
+        String reqUrl = "/" + indexName + "-refs/_doc/" + docId + "?_source=product_lidvid";
+        Request req = new Request("GET", reqUrl);
+        Response resp = null;
+        
+        try
+        {
+            resp = client.performRequest(req);
+        }
+        catch(ResponseException ex)
+        {
+            resp = ex.getResponse();
+            int code = resp.getStatusLine().getStatusCode();
+            // Invalid LIDVID
+            if(code == 404 || code == 405) 
+            {
+                return null;
+            }
+            else
+            {
+                throw ex;
+            }
+        }
+
+        InputStream is = null;
+        
+        try
+        {
+            is = resp.getEntity().getContent();
+            JsonReader rd = new JsonReader(new InputStreamReader(is));
+            
+            rd.beginObject();
+            
+            while(rd.hasNext() && rd.peek() != JsonToken.END_OBJECT)
+            {
+                String name = rd.nextName();
+                if("_source".equals(name))
+                {
+                    return parseRefs(rd);
+                }
+                else
+                {
+                    rd.skipValue();
+                }
+            }
+            
+            rd.endObject();
+        }
+        finally
+        {
+            CloseUtils.close(is);
+        }
+        
+        return null;
+    }
+    
+    
     private String parseProductClassSource(JsonReader rd) throws Exception
     {
         rd.beginObject();
@@ -149,5 +289,45 @@ public class ProductDao
         rd.endObject();
         
         return null;
+    }
+    
+
+    private List<String> parseRefs(JsonReader rd) throws Exception
+    {
+        rd.beginObject();
+
+        while(rd.hasNext() && rd.peek() != JsonToken.END_OBJECT)
+        {
+            String name = rd.nextName();
+            if("product_lidvid".equals(name))
+            {
+                return parseList(rd);
+            }
+            else
+            {
+                rd.skipValue();
+            }
+        }
+        
+        rd.endObject();
+        
+        return null;
+    }
+
+    
+    private List<String> parseList(JsonReader rd) throws Exception
+    {
+        List<String> list = new ArrayList<>();
+        
+        rd.beginArray();
+
+        while(rd.hasNext() && rd.peek() != JsonToken.END_ARRAY)
+        {
+            list.add(rd.nextString());
+        }        
+        
+        rd.endArray();
+        
+        return list;
     }
 }
--- src/test/java/gov/nasa/pds/TestProductDao.java
+++ src/test/java/gov/nasa/pds/TestProductDao.java
@@ -1,5 +1,7 @@
 package gov.nasa.pds;
 
+import java.util.List;
+
 import gov.nasa.pds.harvest.cfg.RegistryCfg;
 import gov.nasa.pds.harvest.dao.ProductDao;
 import gov.nasa.pds.harvest.dao.RegistryManager;
@@ -8,8 +10,45 @@ import gov.nasa.pds.harvest.util.Log4jConfigurator;
 
 public class TestProductDao
 {
-
     public static void main(String[] args) throws Exception
+    {
+        testGetRefDocCount();
+    }
+    
+    
+    public static void testGetRefDocCount() throws Exception
+    {
+        Log4jConfigurator.configure("INFO", "/tmp/t.log");
+        RegistryCfg cfg = createConfiguration();
+
+        try
+        {
+            RegistryManager.init(cfg);
+    
+            ProductDao dao = RegistryManager.getInstance().getProductDao();
+            int val = dao.getRefDocCount("urn:nasa:pds:orex.spice:document::3.0", 'P');
+            System.out.println("Valid OREX docs P = " + val);
+            
+            val = dao.getRefDocCount("urn:nasa:pds:orex.spice:document::3.0", 'S');
+            System.out.println("Valid OREX docs S = " + val);
+
+            val = dao.getRefDocCount("urn:nasa:pds:orex.spice:spice_kernels::8.0", 'S');
+            System.out.println("Valid OREX kernels S = " + val);
+
+            val = dao.getRefDocCount("urn:nasa:pds:orex.spice:document::3.3", 'P');
+            System.out.println("Invalid = " + val);
+            
+            val = dao.getRefDocCount(null, 'P');
+            System.out.println("NULL = " + val);            
+        }
+        finally
+        {
+            RegistryManager.destroy();
+        }
+    }
+    
+    
+    public static void testGetProductClass() throws Exception
     {
         Log4jConfigurator.configure("INFO", "/tmp/t.log");
         RegistryCfg cfg = createConfiguration();
@@ -38,6 +77,35 @@ public class TestProductDao
     }
 
     
+    public static void testGetRefs() throws Exception
+    {
+        Log4jConfigurator.configure("INFO", "/tmp/t.log");
+        RegistryCfg cfg = createConfiguration();
+
+        try
+        {
+            RegistryManager.init(cfg);
+    
+            ProductDao dao = RegistryManager.getInstance().getProductDao();
+            List<String> refs = dao.getRefs("urn:nasa:pds:orex.spice:document::1.0", 'P', 1);
+            System.out.println("Valid OREX docs P = " + refs);
+
+            refs = dao.getRefs("urn:nasa:pds:orex.spice:document::3.0", 'S', 1);
+            System.out.println("Valid OREX docs S = " + refs);
+
+            refs = dao.getRefs("urn:nasa:pds:orex.spice:document::1.0", 'A', 1);
+            System.out.println("Invalid = " + refs);
+
+            refs = dao.getRefs(null, 'A', 1);
+            System.out.println("NULL = " + refs);            
+        }
+        finally
+        {
+            RegistryManager.destroy();
+        }
+    }
+
+    
     private static RegistryCfg createConfiguration()
     {
         RegistryCfg cfg = new RegistryCfg();
@@ -48,4 +116,25 @@ public class TestProductDao
         return cfg;
     }
 
+    
+    private static String escape(String str)
+    {
+        if(str == null) return null;
+        
+        StringBuilder bld = new StringBuilder();
+        for(int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            if(ch == ':')
+            {
+                bld.append("\\:");
+            }
+            else
+            {
+                bld.append(ch);
+            }
+        }
+        
+        return bld.toString();
+    }
 }
